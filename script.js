fc735c49234566bfd88cd2eb4b6717cf9cc57f74
swathimeenal <swathimeenal1994@gmail.com>
2023-09-21 19:03:10 +0530

    update

--- script.js
+++ script.js
@@ -2,11 +2,29 @@ async function foo(){
     try{
         const res = await fetch("https://jsonplaceholder.typicode.com/users")
         const data = await res.json();
-        console.log(data);
+        const datacards = document.getElementById("cards");
         for(var i=0;i<data.length;i++)
         {
-          const div = document.createElement("div");
-         div.innerHTML = `<div class="row row-cols-1 row-cols-md-3 g-4">
+          const a = document.createElement("div");
+         a.innerHTML = `<div class="card mb-3 h-100 shadow" style="width: 18rem;">
+         <div class="card-body">
+           <h5 class="card-title mb-3">Name: ${data[i].name}</h5>
+           <p class="card-text">Email: ${data[i].email}</p>
+           <p class="card-text">Street: ${data[i].address.street}</p>
+           <p class="card-text">City: ${data[i].address.city}</p>
+         </div>
+       </div>`
+       datacards.append(a);
+        }     
+    }
+    catch(error)
+    {
+        console.log(error)
+    }
+    
+}
+foo();
+/*`<div class="row row-cols-1 row-cols-md-4 g-4">
             <div class="col">
                <div class="card">
                   <div class="card-body">
@@ -18,13 +36,4 @@ async function foo(){
                </div>
             </div>
           </div>`
-          document.body.append(div);
-        }     
-    }
-    catch(error)
-    {
-        console.log(error)
-    }
-    
-}
-foo();
+          document.body.append(div);*/
